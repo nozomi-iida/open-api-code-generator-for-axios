37097aa142a689c60dbd94279938857cfcda8206
nozomi-iida <iida19990106@gmail.com>
2022-04-28 09:16:45 +0900

    feat: build

--- src/buildV3.ts
+++ src/buildV3.ts
@@ -1,5 +1,6 @@
 import {OpenAPIV3} from "openapi-types";
 
+// ファイルの生成の前までここでやる
 export const buildV3 = (openapi: OpenAPIV3.Document) => {
   // fileName = operationId
   const files: { fileName: string; methods: string }[] = []
